c73f944ff755e675b3d66c53d4375a1c9d769391
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-29 15:36:33 +0100

    refactored into 1 method with else if statements

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,26 +1,29 @@
 var Javabuzz = function(){};
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
-  if (number % 3 === 0) {
+Javabuzz.prototype.isDivisibleBy = function(number) {
+  if (number % 15 === 0) {
+    return 'fizzbuzz'
+  } else if (number % 3 === 0) {
     return 'fizz'
-  } else {
-    return number
-  }
-};
-
-Javabuzz.prototype.isDivisibleByFive = function(number) {
-  if (number % 5 === 0) {
+  } else if (number % 5 === 0) {
     return 'buzz'
   } else {
     return number
   }
 };
 
-Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-  if (number % 15 === 0) {
-    return 'fizzbuzz'
-  } else {
-    return number
-  }
-};
+// Javabuzz.prototype.isDivisibleByFive = function(number) {
+//   if (number % 5 === 0) {
+//     return 'buzz'
+//   } else {
+//     return number
+//   }
+// };
 
+// Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+//   if (number % 15 === 0) {
+//     return 'fizzbuzz'
+//   } else {
+//     return number
+//   }
+// };
