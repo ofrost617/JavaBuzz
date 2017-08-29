693dac24484e1c26b01e2647e8377e66ee21e60c
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-29 14:21:29 +0100

    first test

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,12 @@
+describe('Fizzbuzz', function() {
+
+  var javabuzz;
+
+  describe('knows when a number is', function() {
+    
+    it('divsible by 3', function() {
+
+      javabuzz = new Javabuzz();
+    });
+  });
+});
