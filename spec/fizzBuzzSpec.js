0709c53f078f1e42fdfa765073c880e2d031628d
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-29 15:28:49 +0100

    three separate is divisible by methods

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,22 +2,50 @@ describe('Fizzbuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divsible by 3', function() {
 
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe('fizz');
     });
+
+
+    it('divisible by 5', function() {
+
+      expect(javabuzz.isDivisibleByFive(5)).toBe('buzz');
+    });
+
+
+    it('divisible by 15', function() {
+
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe('fizzbuzz');
+    });
+
+
   });
 
   describe('knows when a number is NOT', function() {
 
     it('divsible by 3', function() {
 
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+      expect(javabuzz.isDivisibleByThree(1)).toBe(1);
     });
+
+    it('divsible by 5', function() {
+
+      expect(javabuzz.isDivisibleByFive(1)).toBe(1);
+    });
+
+    it('divisible by 15', function() {
+
+      expect(javabuzz.isDivisibleByFifteen(20)).toBe(20);
+    });
+
+
   });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,8 +2,25 @@ var Javabuzz = function(){};
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   if (number % 3 === 0) {
-    return true
+    return 'fizz'
   } else {
-    return false
+    return number
   }
 };
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  if (number % 5 === 0) {
+    return 'buzz'
+  } else {
+    return number
+  }
+};
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  if (number % 15 === 0) {
+    return 'fizzbuzz'
+  } else {
+    return number
+  }
+};
+
