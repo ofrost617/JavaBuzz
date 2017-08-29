0d31ab3995b2d071f45fba230a9e3d1bf404a8b0
David Divilly <d.divilly@gmail.com>
2017-08-29 14:59:30 +0100

    second test and first function

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -3,10 +3,21 @@ describe('Fizzbuzz', function() {
   var javabuzz;
 
   describe('knows when a number is', function() {
-    
+
     it('divsible by 3', function() {
 
       javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divsible by 3', function() {
+
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
 });
---
+++ src/fizzBuzz.js
@@ -0,0 +1,9 @@
+var Javabuzz = function(){};
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  if (number % 3 === 0) {
+    return true
+  } else {
+    return false
+  }
+};
